844c48d168e53c1d49ebcbd3a14a28ec7bc1ac38
Alessandro Sebastiani <sebbalex@gmail.com>
2019-12-21 23:01:40 +0100

    http client refactor

--- httpclient/httpclient.go
+++ httpclient/httpclient.go
@@ -23,95 +23,18 @@ const userAgent = "Golang_italia_backend_bot"
 // GetURL retrieves data, status and response headers from an URL.
 // It uses some technique to slow down the requests if it get a 429 (Too Many Requests) response.
 func GetURL(URL string, headers map[string]string) (HTTPResponse, error) {
-	expBackoffAttempts := 0
-	const timeout = 60 * time.Second
-	const maxBackOffAttempts = 8 // 2 minutes.
-	var err error
-
-	client := http.Client{
-		// Request Timeout.
-		Timeout: timeout,
-	}
-
-	for expBackoffAttempts < maxBackOffAttempts {
-
-		req, err := http.NewRequest("GET", URL, nil)
-		if err != nil {
-			return HTTPResponse{
-				Body:    nil,
-				Status:  ResponseStatus{Text: err.Error() + URL, Code: -1},
-				Headers: nil,
-			}, err
-		}
-
-		// Set headers.
-		for k, v := range headers {
-			req.Header.Add(k, v)
-		}
-
-		// Set special user agent for bot. Note: in github reqs the User-Agent must be set.
-		req.Header.Add("User-Agent", userAgent+"/"+version.VERSION)
-
-		// Perform the request.
-		resp, err := client.Do(req)
-		if err != nil {
-			return HTTPResponse{
-				Body:    nil,
-				Status:  ResponseStatus{Text: err.Error() + URL, Code: -1},
-				Headers: nil,
-			}, err
-		}
-
-		// Check if the request results in http OK.
-		if resp.StatusCode == http.StatusOK {
-			return statusOK(resp)
-		}
-
-		// Check if the request results in http notFound.
-		if resp.StatusCode == http.StatusNotFound {
-			log.Debugf("Status: %s - Resource: %s", resp.Status, URL)
-			return statusNotFound(resp)
-		}
-
-		// Check if the request results in http RateLimit error.
-		if resp.StatusCode == http.StatusTooManyRequests {
-			log.Debugf("Status: %s - Resource: %s", resp.Status, URL)
-			expBackoffAttempts, err = statusTooManyRequests(resp, expBackoffAttempts)
-			if err != nil {
-				return HTTPResponse{
-					Body:    nil,
-					Status:  ResponseStatus{Text: err.Error() + URL, Code: -1},
-					Headers: nil,
-				}, err
-			}
-
-		}
-		// Check if the request result in http Forbidden status.
-		if resp.StatusCode == http.StatusForbidden {
-			log.Debugf("Status: %s - Resource: %s", resp.Status, URL)
-			expBackoffAttempts, err = statusForbidden(resp, expBackoffAttempts)
-			if err != nil {
-				return HTTPResponse{
-					Body:    nil,
-					Status:  ResponseStatus{Text: err.Error() + URL, Code: -1},
-					Headers: nil,
-				}, err
-			}
-		}
-
-	}
-
-	// Generic invalid status code.
-	return HTTPResponse{
-		Body:    nil,
-		Status:  ResponseStatus{Text: "Invalid Status Code: " + URL, Code: -1},
-		Headers: nil,
-	}, err
+	return Request(URL, "GET", headers, nil)
 }
 
 // PostURL retrieves data, status and response headers from an URL.
 // It uses some technique to slow down the requests if it get a 429 (Too Many Requests) response.
 func PostURL(URL string, headers map[string]string, body io.Reader) (HTTPResponse, error) {
+	return Request(URL, "POST", headers, body)
+}
+
+// Request retrieves data, status and response headers from an URL.
+// It uses some technique to slow down the requests if it get a 429 (Too Many Requests) response.
+func Request(URL string, verb string, headers map[string]string, body io.Reader) (HTTPResponse, error) {
 	expBackoffAttempts := 0
 	const timeout = 60 * time.Second
 	const maxBackOffAttempts = 8 // 2 minutes.
@@ -124,7 +47,7 @@ func PostURL(URL string, headers map[string]string, body io.Reader) (HTTPRespons
 
 	for expBackoffAttempts < maxBackOffAttempts {
 
-		req, err := http.NewRequest("POST", URL, body)
+		req, err := http.NewRequest(verb, URL, body)
 		if err != nil {
 			return HTTPResponse{
 				Body:    nil,
